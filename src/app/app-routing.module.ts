eabf5af56bb143331b1a98af63371b8debdbf795
Nam Nguyen <nnguyen17@hawk.iit.edu>
2022-09-20 21:03:46 -0500

    fix the creation of modules and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'AboutRoute', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) }, { path: 'GalleryRoute', loadChildren: () => import('./gallery/gallery.module').then(m => m.GalleryModule) }, { path: 'UpdateRoute', loadChildren: () => import('./update/update.module').then(m => m.UpdateModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
