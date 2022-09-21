a37390abf128679469476b801c65e5f14888215f
Nam Nguyen <nnguyen17@hawk.iit.edu>
2022-09-20 20:52:32 -0500

    add bootstrap and create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about/about.component';
+import { GalleryComponent } from './gallery/gallery/gallery.component';
+import { UpdateComponent } from './update/update/update.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    GalleryComponent,
+    UpdateComponent
   ],
   imports: [
     BrowserModule,
